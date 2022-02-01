7d13a0ef9a3dd1cdc2d90523ee195e3159d4da83
Kevin Rogers <kevinarogers1@gmail.com>
2022-02-01 14:28:25 -0500

    commit tree 27184f2cf599a0eddae96ec2b1a37ca9b7638fc4

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+function reverse(word) {
+  let reverse = "";
+
+  for(let i = word.length - 1; i >= 0; i-- ) {
+    reverse+= word[i]
+  } 
+  return reverse;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    const reversedWord = reverse(word)
+
+    return (reversedWord === word)
 }
 
 /* 
   Add your pseudocode here
+  Reverse a string and if it's exactly the same as the original string it is a Palindrome
+
+  Check whether a string is a Palindrome by using the reversed version and 
+  if the reversed is equal to the original 
+    return true 
+  otherwise
+    return false  
+  
 */
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
+First function created to reverse the word. Utilizing for loop to decrement/grab each letter from end to begining. Placing it in the empty holder  we created. "reverse" 
+The reversed word is passed into our isPalindrome function to be tested for similarity to the original word
+The reversedWord variable recieves the reversed word and we return whether it's true or false when compared. 
+
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +45,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
